98f3a5f66fdc6e3ebbf5dadb702ed9449b4088ca
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-04 18:38:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,6 @@ class Application
   def call (env)
     resp = Rack::Response.new 
     now = Time.new 
-    now = time.hour
      if now < 12
        print "It is ""#{now}"
        resp.write "Good Morning!"
