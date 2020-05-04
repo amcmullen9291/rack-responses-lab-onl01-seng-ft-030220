6f7b474581e822e83d71838aa90788bff7704a92
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-04 19:01:26 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,10 +5,8 @@ class Application
     now = clock.to_a
     hour = now[2]
      if hour < 12
-       resp.write "It is ""#{clock}"
        resp.write "Good Morning!"
      else
-       resp.write "It is ""#{clock}" 
        resp.write "Good Afternoon!"
      end
      resp.finish
