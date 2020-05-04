4fb5d50d5d3f71a48e243042a78cd1486ed3e1a7
anthony mcmullen <jondouglasheit@gmail.com>
2020-05-04 18:56:16 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,9 +4,7 @@ class Application
     resp = Rack::Response.new 
     clock = Time.now
     now = clock.to_a
-    now = now.split(",")
-    hour = now[4]
-    # hour = hour.to_i
+    hour = now[2]
      if hour < 12
        print "It is ""#{clock}"
        resp.write "Good Morning!"
